de79955deb9b93b9d582d4accbc27d587ca1c5fe
ghostwan <ghoswtan@gmail.com>
2014-09-28 11:39:17 -0700

    ignore class

--- desktop/HelloDesktop/src/com/aldebaran/demo/MainScenar.java
+++ desktop/HelloDesktop/src/com/aldebaran/demo/MainScenar.java
@@ -5,7 +5,7 @@ package com.aldebaran.demo;
  */
 public class MainScenar {
 
-	private static String ROBOT_IP = "192.168.0.178";
+	private static String ROBOT_IP = "";
 
 	public static void main(String[] args) {
 		new Scenar1().start(ROBOT_IP);
